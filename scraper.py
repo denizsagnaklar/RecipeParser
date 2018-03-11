0b7722d63281d2ddb5556f23da0262c3cd1cbe3c
Tristan <tlitre@gmail.com>
2018-03-11 18:40:59 -0500

    Got recipe scraper working

--- scraper.py
+++ scraper.py
@@ -4,8 +4,6 @@ from bs4 import BeautifulSoup
 def scrapeRecipe(url):
     page = requests.get(url)
     soup = BeautifulSoup(page.content, 'html.parser')
-    #ingredientsLeft = soup.find_all(id="lst_ingredients_1")
-    #ingredientsRight = soup.find_all(id="id=lst_ingredients_2")
     ingredients = []
     ingredientSoup = soup.find_all("span", itemprop="ingredients")
     ingredients = [i.text for i in ingredientSoup]
