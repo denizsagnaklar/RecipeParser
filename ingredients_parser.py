b57f44a688c8f73e57c0e59da5e06a1ab92fb3a3
noradiegwu <nneomaoradiegwu2019@u.northwestern.edu>
2018-03-12 13:13:12 -0500

    get list of stripped down names, some fxn descriptions

--- ingredients_parser.py
+++ ingredients_parser.py
@@ -5,9 +5,9 @@ import re
  # repo: https://github.com/tlitre/RecipeParser.git
 '''
 
-ingredients = ["1/8 teaspoon hot pepper sauce", "4 bone-in chicken breast halves, with skin",
+ingredients_list = ["1/8 teaspoon hot pepper sauce", "4 bone-in chicken breast halves, with skin",
 "12 pounds spaghetti", "1 clove crushed garlic", "1/2 teaspoon salt", "2 1/2 cups white sugar",
-"2 tablespoons butter", "2 1/3 teaspoons milk", "4 3/5 ounces goat's milk"]
+"2 tablespoons melted butter", "2 1/3 teaspoons milk", "4 3/5 ounces goat milk"]
 
 measurements = [r'([a-z]+)spoons?', r'cloves?', r'cups?', r'pounds?', r'ounces?']
 
@@ -77,6 +77,16 @@ def find_ingredient_descriptor(name):
     return descriptor
 
 
+'''
+Input: the ingredient string ex. "4 3/5 ounces goat milk"
+Prints: 4 3/5 ounces goat milk
+        NAME: goat milk
+        QTY: 4.6
+        MSRMNT: ounces
+        DESC: goat
+        PREP:
+Prints out ingredient info
+'''
 def print_ingredient(ing):
         i_name = find_ingredient_name(i)
         print "NAME:", find_ingredient_name(i)
@@ -85,10 +95,25 @@ def print_ingredient(ing):
         print "DESC:", find_ingredient_descriptor(i_name)
         print "PREP:", find_ingredient_preparation(i),'\n'
 
-for i in ingredients:
+
+'''
+Input: List of full ingredients (ex. ["1/8 teaspoon hot pepper sauce", "4 3/5 ounces goat milk"])
+Return: list of stripped ingredient names (ex. ["pepper sauce", "milk"])
+
+Use this to replace ingredients in the prep directions
+'''
+def get_ingredient_names(ingredients_list):
+    names = []
+    for i in ingredients_list:
+        s = find_ingredient_name(i)
+        names.append(s.replace(find_ingredient_descriptor(s), "").strip())
+    return names
+
+for i in ingredients_list:
     print i
     print_ingredient(i)
 
+print get_ingredient_names(ingredients_list)
 #
 # class Ingredient:
 #     def __init__(self, details):
