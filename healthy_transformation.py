7519c3c76ba434434319f0d426a4285bceddf9ce
noradiegwu <nneomaoradiegwu2019@u.northwestern.edu>
2018-03-12 12:17:21 -0500

    healthy transfor

---
+++ healthy_transformation.py
@@ -0,0 +1,14 @@
+# healthy transform
+# find healthy substitutes
+# have a reasoning for making the recipe healthier
+# maybe fewer calorie in ingredients
+def to_healthy():
+    healthy_subs_dict = {
+
+    }
+
+    '''
+    If there is an ingredient in the text, find it in the ingredient dict and sub it w/ its value
+
+
+    '''
