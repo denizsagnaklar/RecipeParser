a07572736a47fe5605afd3c62c3b408a062132cd
Tristan <tlitre@gmail.com>
2018-03-12 12:04:25 -0500

    Add technique list

---
+++ techniques.py
@@ -0,0 +1,3 @@
+def getTechniques:
+	techniques = ['Bake', 'Boil', 'Braise', 'Brown', 'Caramelize', 'Deglaze', 'Fry', 'Grill', 'Microwave', 'Pan fry', 'Poach', 'Pressure cook', 'Reduce', 'Sauté', 'Sear', 'Simmer', 'Slow cook', 'Sous-vide', 'Steam', 'Stew', 'Stir fry', 'Sweat']
+	return techniques
